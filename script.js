8627fd62b7b9575043e5a4041d3e084bc859bde0
Fahmiali99 <79827631+Fahmiali99@users.noreply.github.com>
2021-12-17 20:12:50 +0700

    work with  me

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const menuTogle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
+const menuTogle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
 
-menuTogle.addEventListener('click', function () {
-    nav.classList.toggle('slide')
+menuTogle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
